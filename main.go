7d5d8f728806bd68262a100332ce3c16c8449c80
Soulou <leo@unbekandt.eu>
2013-12-09 00:31:24 +0000

    Update Renderer from martini-contrib

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 	m := martini.Classic()
-	m.Use(render.Renderer("templates"))
+	m.Use(render.Renderer(
+		render.Options{
+			Directory: "templates",
+		},
+	))
 
 	m.Get("/", func(r render.Render) {
 		r.HTML(200, "index", nil)
